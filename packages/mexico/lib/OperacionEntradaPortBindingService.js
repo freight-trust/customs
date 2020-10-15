56bda433c6662a438c896b03b7335d3ecc488b60
sam bacha <sam@freighttrust.com>
2020-10-15 06:10:52 -0700

    Create OperacionEntradaPortBindingService.js

---
+++ packages/mexico/lib/OperacionEntradaPortBindingService.js
@@ -0,0 +1,434 @@
+'use strict';
+
+
+/**
+ * ceAgregacionMercancia
+ *
+ * body CeAgregacionMercancia 
+ * returns ceAgregacionMercanciaResponse
+ **/
+exports.ceAgregacionMercancia = function(body) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * ceDesagregacionMercancia
+ *
+ * body CeDesagregacionMercancia 
+ * returns ceDesagregacionMercanciaResponse
+ **/
+exports.ceDesagregacionMercancia = function(body) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * consolidacionMaster
+ *
+ * body ConsolidacionMaster 
+ * returns consolidacionMasterResponse
+ **/
+exports.consolidacionMaster = function(body) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * desconsolidacionMaster
+ *
+ * body DesconsolidacionMaster 
+ * returns desconsolidacionMasterResponse
+ **/
+exports.desconsolidacionMaster = function(body) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * enviaPendientesP0
+ *
+ * body EnviaPendientesP0 
+ * returns enviaPendientesP0Response
+ **/
+exports.enviaPendientesP0 = function(body) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * generaGuiaHouseP0Resp
+ *
+ * body GeneraGuiaHouseP0Resp 
+ * returns generaGuiaHouseP0RespResponse
+ **/
+exports.generaGuiaHouseP0Resp = function(body) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * generaGuiaP0Resp
+ *
+ * body GeneraGuiaP0Resp 
+ * returns generaGuiaP0RespResponse
+ **/
+exports.generaGuiaP0Resp = function(body) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * generaP0Resp
+ *
+ * body GeneraP0Resp 
+ * returns generaP0RespResponse
+ **/
+exports.generaP0Resp = function(body) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * getBitacora
+ *
+ * body GetBitacora 
+ * returns getBitacoraResponse
+ **/
+exports.getBitacora = function(body) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * incidenciaMercancia
+ *
+ * body IncidenciaMercancia 
+ * returns incidenciaMercanciaResponse
+ **/
+exports.incidenciaMercancia = function(body) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * ingresoFlujoAlterno
+ *
+ * body IngresoFlujoAlterno 
+ * returns ingresoFlujoAlternoResponse
+ **/
+exports.ingresoFlujoAlterno = function(body) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * ingresoParcial
+ *
+ * body IngresoParcial 
+ * returns ingresoParcialResponse
+ **/
+exports.ingresoParcial = function(body) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * ingresoSimple
+ *
+ * body IngresoSimple 
+ * returns ingresoSimpleResponse
+ **/
+exports.ingresoSimple = function(body) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * ingresoSinID
+ *
+ * body IngresoSinID 
+ * returns ingresoSinIDResponse
+ **/
+exports.ingresoSinID = function(body) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * notificacionIngresoMercancia
+ *
+ * body NotificacionIngresoMercancia 
+ * returns notificacionIngresoMercanciaResponse
+ **/
+exports.notificacionIngresoMercancia = function(body) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * obtenDetalleIdAsociado
+ *
+ * body ObtenDetalleIdAsociado 
+ * returns obtenDetalleIdAsociadoResponse
+ **/
+exports.obtenDetalleIdAsociado = function(body) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * obtenIdAsociados
+ *
+ * body ObtenIdAsociados 
+ * returns obtenIdAsociadosResponse
+ **/
+exports.obtenIdAsociados = function(body) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * rechazo
+ *
+ * body Rechazo 
+ * returns rechazoResponse
+ **/
+exports.rechazo = function(body) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * salidaPedimento
+ *
+ * body SalidaPedimento 
+ * returns salidaPedimentoResponse
+ **/
+exports.salidaPedimento = function(body) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * salidaTransferencia
+ *
+ * body SalidaTransferencia 
+ * returns salidaTransferenciaResponse
+ **/
+exports.salidaTransferencia = function(body) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * subdivisionMercancia
+ *
+ * body SubdivisionMercancia 
+ * returns subdivisionMercanciaResponse
+ **/
+exports.subdivisionMercancia = function(body) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * traspaleoContenedor
+ *
+ * body TraspaleoContenedor 
+ * returns traspaleoContenedorResponse
+ **/
+exports.traspaleoContenedor = function(body) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * traspasoMercancia
+ *
+ * body TraspasoMercancia 
+ * returns traspasoMercanciaResponse
+ **/
+exports.traspasoMercancia = function(body) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * validacionGeneral
+ *
+ * body ValidacionGeneral 
+ * returns validacionGeneralResponse
+ **/
+exports.validacionGeneral = function(body) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
